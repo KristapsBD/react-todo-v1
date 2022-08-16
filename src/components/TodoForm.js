47c9c78008b6935bde68d74051007726f6833da6
Kristaps Briks <kristaps.briks@inbox.lv>
2022-08-16 19:52:30 +0300

    Proper css for update form

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -27,8 +27,8 @@ function TodoForm(props) {
   return (
     <form className='todo-form' onSubmit={handleSubmit}>
         {props.edit ? (
-            <> <input type='text' placeholder='Update todo' value={input} name='text' className='todo-input' onChange={handleChange} ref={inputRef}/>
-            <button className='todo-button'>Update todo</button> </>
+            <> <input type='text' placeholder='Update todo' value={input} name='text' className='todo-input edit' onChange={handleChange} ref={inputRef}/>
+            <button className='todo-button edit'>Update todo</button> </>
         ) : (
             <> <input type='text' placeholder='Add a todo' value={input} name='text' className='todo-input' onChange={handleChange} ref={inputRef}/>
             <button className='todo-button'>Add todo</button> </>
