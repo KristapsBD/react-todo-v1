1612b384b6e51589278cd14ace0437894a047e75
Kristaps Briks <kristaps.briks@inbox.lv>
2022-08-16 19:25:13 +0300

    Display todos on page

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+import Todo from './Todo'
 import TodoForm from './TodoForm'
 
 function TodoList() {
@@ -11,12 +12,24 @@ function TodoList() {
     const newTodos = [todo, ...todos]
 
     setTodos(newTodos)
+  };
+
+  const completeTodo = id => {
+    let updatedTodos = todos.map(todo => {
+        if(todo.id === id) {
+            todo.isComplete = !todo.isComplete
+        }
+        return todo;
+    });
+    
+    setTodos(updatedTodos);
   }
 
   return (
     <div>
         <h1>Whats the plan for today?</h1>
         <TodoForm onSubmit={addTodo}></TodoForm>
+        <Todo todos={todos} completeTodo={completeTodo}/>
     </div>
   )
 }
